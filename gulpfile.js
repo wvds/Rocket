29be827987a57c968d3abc00ccc77908c476945a
Nicky Lenaers <nickylenaers@gmail.com>
2014-06-12 13:51:50 +0200

    Gulp Compass
    Installed Gulp Compass and updated the .gitignore file to ignore the
    .sass-cache folder.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
+var compass = require('gulp-compass');
 
 gulp.task('lint', function() {
 	return gulp.src('comp/js/*.js')
@@ -8,8 +9,19 @@ gulp.task('lint', function() {
 		.pipe(jshint.reporter('default'));
 });
 
+gulp.task('compass', function() {
+	return gulp.src('comp/sass/*.scss')
+		.pipe(compass({
+			config_file: './config.rb',
+			css: 'public/css',
+			sass: 'comp/sass'
+		}))
+		.pipe(gulp.dest('public/css'));
+});
+
 gulp.task('watch', function() {
 	gulp.watch('comp/js/*.js', ['lint']);
+	gulp.watch('comp/sass/*.scss', ['compass']);
 });
 
 gulp.task('default', ['watch']);
