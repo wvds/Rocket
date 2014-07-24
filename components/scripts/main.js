902551c54d737658255408ed3b160c1ba7960cfd
Nicky Lenaers <nickylenaers@gmail.com>
2014-07-24 09:44:06 +0200

    Updating lay-out with flexbox modules

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -1,6 +1,17 @@
 // Browserify Requirements
 var $ = require('jquery');
+require('jquery-ui');
 
+// DOM Loaded
 $(function() {
 	console.log("Hello World Rocket!");
+	
+	/*$('#config').resizable({
+		ghost: true,
+		handles: "e, w"
+	});
+	Docs:
+	http://api.jqueryui.com/resizable/
+	http://viralpatel.net/blogs/jquery-resizable-draggable-resize-drag-tutorial-example/
+	*/
 });
