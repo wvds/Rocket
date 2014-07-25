914c37e58495f41df76df42ae4c63d3b587e4155
Nicky Lenaers <nickylenaers@gmail.com>
2014-07-25 16:55:55 +0200

    Touch Support
    Started to add some touch support. Still conflicting with the FTScroller plugin though.

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -1,13 +1,18 @@
 'use strict';
 // Browserify Requirements
 var $ = require('jquery');
-require('jquery-ui');
+//require('jquery-ui');
 //var FTScroller = require('ftscroller');
 
 // DOM Loaded
 $(function() {
 	
+	// Support Table	
+	console.log("Support Touch: " + support.touch());
+	console.log("Support Drag and Drop: " + support.draganddrop());
+	console.log("Support Web Socket: " + support.websocket());
 	
+	// FT Scroller
 	var container = document.getElementById('panels'),
 		scroller = new FTScroller(container, {
 			scrollingY: false,
@@ -17,14 +22,66 @@ $(function() {
 			bouncing: false
 		});
 	
-	/*$('#config').resizable({
-		ghost: true,
-		handles: "e, w"
-	});
-	Docs:
-	http://api.jqueryui.com/resizable/
-	http://viralpatel.net/blogs/jquery-resizable-draggable-resize-drag-tutorial-example/
-	*/
+	// Drag & Drop
+	var element = document.getElementById('inspector');
 	
-	//$('#inspector').draggable();
+	element.addEventListener('dragstart', handleDragStart, false);
+	element.addEventListener('drag', handleDrag, false);
+	
+	element.addEventListener('touchstart', handleTouchStart, false);
+	element.addEventListener('touchmove', handleTouchMove, false);
+	
+	//element.addEventListener('dragend', handleDragEnd, false);
+	
+	
+	//dropzone.addEventListener('dragenter', handleDropEvent, false);
+	//dropzone.addEventListener('dragleave', handleDropEvent, false);
+	document.documentElement.addEventListener('dragover', handleDragOver, false); // Works like the 'drag' event on draggable object
+	//dropzone.addEventListener('drop', handleDrop, false);
+	
+	function handleTouchStart(e) {
+		console.log("touched...");
+		$('#inspector').addClass('loose');
+		console.log("PageX Touch: " + e.touches[0].pageX);
+	}
+	
+	function handleTouchMove(e) {
+		console.log("touche moving...");
+		$('#' + this.id).css({
+			'top': e.touches[0].pageY,
+			'left': e.touches[0].pageX
+		});
+	}
+	
+	function handleDragStart(e) {
+		var id = this.id;
+		$('#inspector').addClass('loose');
+		console.log("Drag start with id: " + id);
+		console.log("ClientX: " + e.clientX);
+	}
+	
+	function handleDrag(e) {
+		$('#' + this.id).css({
+			'top': e.clientY,
+			'left': e.clientX
+		});
+	}
+	
+	function handleDragEnd(e) {
+		console.log("Drag end");
+	};
+	
+	function handleDrop(e) {
+		if(e.preventDefault) e.preventDefault(); // Firefox sometimes displays the image in a new windows when dropped
+		console.log("Drag dropped");
+	};
+	
+	// Neccessary for the drop event to occur!
+	function handleDragOver(e) {
+		
+		//console.log("sup");
+		if(e.preventDefault) e.preventDefault();
+		return false;
+	}
+	
 });
---
+++ components/scripts/support.js
@@ -0,0 +1,26 @@
+var support = new function() {
+	
+	this.touch = function() {
+		return !!('ontouchstart' in window || 'onmsgesturechange' in window);
+	};
+	
+	// HTML5: Drag & Drop
+	this.draganddrop = function() {
+		if(navigator.appName == 'Microsoft Internet Explorer') {
+			var ua = navigator.userAgent;
+			var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+			if(re.exec(ua) !== null) {
+				var rv = parseFloat(RegExp.$1);
+				if(rv >= 6.0) return true;
+			}
+			return false;
+		}
+		if('draggable' in document.createElement('span')) return true;
+		return false;
+	};
+	
+	// HTML5: Web Socket
+	this.websocket = function() {
+		return !(typeof(WebSocket) != "function" );
+	};
+};
